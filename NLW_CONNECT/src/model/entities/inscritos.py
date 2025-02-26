eacbfa10a4d55f4d3cc7f6de9cb1eb4d4cbb23da
IsaquePerezRodrigues <isaquenovo127@gmail.com>
2025-02-26 18:07:43 -0300

    feat: Implementing subscribers repository

--- NLW_CONNECT/src/model/entities/inscritos.py
+++ NLW_CONNECT/src/model/entities/inscritos.py
@@ -5,7 +5,7 @@ class Inscritos(Base):
     __tablename__ = "Inscritos"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    nome = Column(String, nullabe=False)
+    nome = Column(String, nullable=False)
     email = Column(String, nullable=False)
     link = Column(String, nullable=True)
     evento_id = Column(Integer, ForeignKey("Eventos.id"))
