8a838752489cbf6893d85ae3616874cca803747f
IsaquePerezRodrigues <isaquenovo127@gmail.com>
2025-03-18 10:59:10 -0300

    feat: implementing http_types

--- NLW_CONNECT/src/main/routes/event.py
+++ NLW_CONNECT/src/main/routes/event.py
@@ -1,8 +1,15 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, Request
 
 event_route_bp = Blueprint("event_route", __name__)
 
+from src.http_types.http_response import HttpResponse
+from src.http_types.http_request import HttpRequest  
+
 @event_route_bp.route("/event", methods=["POST"])
 def create_new_event():
-    return jsonify({ "estou": "aqui" }), 201
+    HttpRequest = HttpRequest(body=Request.json)
+    
+    Http_Response = HttpResponse(body = { "estou": "aqui" }, status_code = 201)
+
+    return jsonify(Http_Response.body, Http_Response.status_code)
 
