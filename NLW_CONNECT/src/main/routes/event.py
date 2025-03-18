4086f736987f6fd84794439e2a273a68d2091705
IsaquePerezRodrigues <isaquenovo127@gmail.com>
2025-03-18 11:27:29 -0300

    feat: Implementing Validators

--- NLW_CONNECT/src/main/routes/event.py
+++ NLW_CONNECT/src/main/routes/event.py
@@ -1,15 +1,17 @@
-from flask import Blueprint, jsonify, Request
+from flask import Blueprint, jsonify, request
 
 event_route_bp = Blueprint("event_route", __name__)
 
+from src.validators.events_creator_validator import events_creator_validator
 from src.http_types.http_response import HttpResponse
 from src.http_types.http_request import HttpRequest  
 
 @event_route_bp.route("/event", methods=["POST"])
 def create_new_event():
-    HttpRequest = HttpRequest(body=Request.json)
+    events_creator_validator(request)    
+    http_Request = HttpRequest(body=request.json)
     
-    Http_Response = HttpResponse(body = { "estou": "aqui" }, status_code = 201)
+    http_Response = HttpResponse(body = { "estou": "aqui" }, status_code = 201)
 
-    return jsonify(Http_Response.body, Http_Response.status_code)
+    return jsonify(http_Response.body, http_Response.status_code)
 
