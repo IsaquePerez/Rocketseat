f2c3bb53342c55618b4085cb2b1a7a99d9889028
IsaquePerezRodrigues <isaquenovo127@gmail.com>
2025-03-18 10:32:10 -0300

    feat: add Interfaces

---
+++ NLW_CONNECT/src/model/repositories/interfaces/subscribers_repository.py
@@ -0,0 +1,12 @@
+from src.model.entities.inscritos import Inscritos
+from abc import ABC, abstractmethod
+
+class SubscribersRepository(ABC):
+
+    @abstractmethod
+    def insert(self, subscriber_infos: dict) -> None: 
+        pass
+    
+    @abstractmethod
+    def select_subscriber(self, email: str, evento_id: int) -> Inscritos: 
+        pass
